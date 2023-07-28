bca85c53bbe1b2b0f6cd180773db6f21bf7962da
inkuksuss <inkuksuss@naver.com>
2023-07-28 17:53:07 +0900

    cors 설정 변경

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,28 @@ import axios from 'axios';
 
 const doTest = () => {
   axios
-    .post('http://localhost:8080/member/test', {})
+    .post('http://18.222.21.148:8080/api/member/test', {})
     .then((res) => console.log(res))
     .catch((e) => console.log(e));
+};
+
+const doTest2 = () => {
+  axios
+      .post('http://middleware:8080/api/member/test', {})
+      .then((res) => console.log(res))
+      .catch((e) => console.log(e));
+};
+const doTest3 = () => {
+  axios
+      .post('http://127.0.0.1:8080/api/member/test', {})
+      .then((res) => console.log(res))
+      .catch((e) => console.log(e));
+};
+const doTest4 = () => {
+  axios
+      .post('http://localhost:8080/api/member/test', {})
+      .then((res) => console.log(res))
+      .catch((e) => console.log(e));
 };
 </script>
 
@@ -16,7 +35,9 @@ const doTest = () => {
   </header>
 
   <div>프로젝트 시작</div>
-  <button @click="doTest"></button>
+  <button @click="doTest">test1</button>
+  <button @click="doTest3">test3</button>
+  <button @click="doTest4">test4</button>
 
   <RouterView />
 </template>
